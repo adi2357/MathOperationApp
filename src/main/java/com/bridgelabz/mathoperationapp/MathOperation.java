4d6fbfc7bbd7997d8b8ebb089b34b3ecf35a81f4
Aditya Verma <aaadarths2357@gmail.com>
2020-10-01 11:33:56 +0530

    UC1.1 : Lambda Expression for Math Functions

--- src/main/java/com/bridgelabz/mathoperationapp/MathOperation.java
+++ src/main/java/com/bridgelabz/mathoperationapp/MathOperation.java
@@ -1,8 +1,28 @@
 package com.bridgelabz.mathoperationapp;
 
+import java.util.Scanner;
+
+@FunctionalInterface
+interface MathFunction{
+	int calculate(int i,int j);
+}
 public class MathOperation {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Math Operation App");
+		Scanner sc=new Scanner(System.in);
+		
+		MathFunction add=(i,j)->i+j;
+		MathFunction subtract=(i,j)->i-j;
+		MathFunction multiply=(i,j)->i*j;
+		MathFunction divide=(i,j)->(j!=0)?(i/j):0;
+		
+		System.out.println("Enter two numbers for arithematic operations:");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		
+		System.out.println("Addition is : "+add.calculate(a, b));
+		System.out.println("Substration is : "+subtract.calculate(a, b));
+		System.out.println("Multiplication is : "+multiply.calculate(a, b));
+		System.out.println("Division is : "+divide.calculate(a, b));
 	}
 
 }
